5c628f01ca86d470025766db5ac79fbc3336be03
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-09-09 21:28:08 -0500

    Add files via upload
    Time Complexity: 
    Worst and Average Case : O(n^2)
    Best Case: O(n)

---
+++ insertionSort.py
@@ -0,0 +1,15 @@
+def insertionSort(array):
+	for i in range(1, len(array)):
+	key = array[i]
+	j = i - 1
+	while j >= 0 and key < array[j]:
+		array[j+1] = array[j]
+		j -= 1
+	array[j+1] = key
+
+
+array =[12, 11, 13, 5, 6]
+insertionSort(array)
+print("Sorted Array is:" )
+for i in range(len(array)):
+	print(array[i])
